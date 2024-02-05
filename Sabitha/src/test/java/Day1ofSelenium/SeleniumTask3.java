9b347e39573355fb5a2fc63b85213cc07d0eb771
Sabitha234 <71449944+Sabitha234@users.noreply.github.com>
2024-02-05 14:32:43 +0530

    "Do changes"

--- Sabitha/src/test/java/Day1ofSelenium/SeleniumTask3.java
+++ Sabitha/src/test/java/Day1ofSelenium/SeleniumTask3.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class Task2 {
+public class SeleniumTask3 {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
